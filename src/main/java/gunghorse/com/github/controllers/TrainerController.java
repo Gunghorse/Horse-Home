a0545bc4f09ab85f1b72b3ef25f0484b1e93621f
Dimonium-239 <dymitr.kuzmin@gmail.com>
2021-02-21 15:33:24 +0100

    Add CORS for ReactApp

---
+++ src/main/java/gunghorse/com/github/controllers/TrainerController.java
@@ -0,0 +1,14 @@
+package gunghorse.com.github.controllers;
+
+import gunghorse.com.github.model.user.role.Role;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping(path="/trainer")
+public class TrainerController {
+    public TrainerController() {
+    }
+}
---
+++ src/main/java/gunghorse/com/github/services/TrainerService.java
@@ -0,0 +1,9 @@
+package gunghorse.com.github.services;
+
+import gunghorse.com.github.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TrainerService {
+
+}
